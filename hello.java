dfd12298c2743f7ac6d7804d527044e55b8b00b0
yogitasoma <117525973+yogitasoma@users.noreply.github.com>
2022-11-06 01:50:24 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.print ("the sum of given numbers are : ");
+        int num1=4;
+        int num2=3;
+        int num3=2;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+
+    
+}
+
+//naming coonventions
+ //for classes :PascalNamingConventions
+ //for funtions: camelCaseNamingConventions
